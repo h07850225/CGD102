afba23bb4ef30482d395af988a451f18449e2f8f
h07850225 <h07850225@gmail.com>
2022-07-25 18:00:18 +0800

    2022/07/25upload

---
+++ JavaScirpt/index.js
@@ -0,0 +1,10 @@
+function init(){
+document.getElementsByClassName("login").onclick = function(){
+    let loginsection = document.getElementsByClassName("loginsection");
+    if((loginsection.classList.contains("hidden")) == true){
+        loginsection.classList.remove("hidden");
+    }else{//沒有hidden
+        loginsection.classList.add("hidden");
+    }
+}
+}
